ff4ea4cedaf81e59391b372ea65306c31ad3328f
QiyaoShn <18272214732@163.com>
2020-10-26 23:16:16 +0800

    数据流

---
+++ 11C/homework_1/FileInputStreamTest.java
@@ -0,0 +1,18 @@
+package homework_1;
+import java.io.*;
+public class FileInputStreamTest {
+	public static void main(String[] args) {
+		try {
+			FileInputStream in = new FileInputStream("myFile.dat");
+			while(in.available()>0)
+				System.out.println(in.read()+" ");
+			in.close();
+		}catch (FileNotFoundException e) {
+			System.out.println("Error: Cannot open file for reading.");
+		}catch (EOFException e) {
+			System.out.println("Error: EOF encountered, file may be corrupted.");
+		}catch(IOException e) {
+			System.out.println("Error: Cannot read from file.");
+		}
+	}
+}
---
+++ 11C/homework_1/FileOutputStreamTest.java
@@ -0,0 +1,20 @@
+package homework_1;
+import java.io.*;
+public class FileOutputStreamTest {
+	public static void main(String[] args) {
+		try {
+			FileOutputStream out = new FileOutputStream("myFile.dat");
+			out.write('H');
+			out.write(69);
+			out.write(76);
+			out.write('L');
+			out.write('O');
+			out.write('!');
+			out.close();
+		}catch(FileNotFoundException e) {
+			System.out.println("Error: Cannot open file for writing.");
+		}catch(IOException e) {
+			System.out.println("Error: Cannot write to file.");
+		}
+	}
+}
